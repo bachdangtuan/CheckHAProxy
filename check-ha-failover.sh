82b4182585f3f4cd7acf8492e2bcd449c1020b99
DESKTOP-FPTSOG5\DarkTuan <bachdangtuan93@gmail.com>
2023-03-07 19:55:31 +0700

    suawr laij chut

--- check-ha-failover.sh
+++ check-ha-failover.sh
@@ -3,11 +3,27 @@ source ./variable/setting.sh
 
 
 echo $SERVICE
-status=$(systemctl is-active haproxy | awk '{print $1}')
+status=$(systemctl is-active $SERVICE | awk '{print $1}')
 
+echo $status
 
-if [$status == 'active'] ; then
+if [ "$status" != "active" ]; then
     echo "Service đang hoạt động"
 else
     echo "Service đang không hoạt động"
 fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
